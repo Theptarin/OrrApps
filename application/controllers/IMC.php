1de9e0128a19b4c8d107dd16a6e8ac0d48f2f348
suchartbu <suchartbu@gmail.com>
2018-07-30 17:51:00 +0700

    update IMC

--- application/controllers/IMC.php
+++ application/controllers/IMC.php
@@ -33,27 +33,33 @@ class IMC extends MY_Controller {
     public function icd10_opd() {
         $crud = $this->acrud;
         $crud->setTable('imc_icd10_opd');
-        $crud->where(['hn'=>0]);
-        $crud->columns(['visit_date', 'vn', 'hn']);
+        //$crud->where(['hn' => 0]);
+        $crud->columns(['visit_date', 'vn', 'hn', 'signature_opd']);
         $crud->setRelationNtoN('opd_principal_diag', 'imc_opd_principal_diag', 'imc_icd10_code', 'icd10_opd_id', 'icd10_code_id', '{code} {name_en}');
+        //print_r($this->_getDoctorName());
+        if ($crud->getState() === 'Initial') {
+            //$crud->fieldType('hn', 'dropdown_search', $this->_getPatient());
+            $crud->fieldType('signature_opd', 'dropdown_search', $this->_getDoctor());
+        }
+
         //$crud->setRelation('signature_opd', 'ttr_hims.doctor_name', '{fname} {lname} [ {doctor_id} ]');
         $output = $crud->render();
         $this->setMyView($output);
     }
-    
-    public function icd10_ipd(){
+
+    public function icd10_ipd() {
         $crud = $this->acrud;
         $crud->setTable('imc_icd10_ipd');
         $crud->columns(['discharge_date', 'an', 'hn']);
-        $crud->setRelationNtoN('ipd_principal_diag','imc_ipd_principal_diag','imc_icd10_code','icd10_ipd_id','icd10_code_id','{code} {name_en}');
-        $crud->setRelationNtoN('ipd_comorbidity_diag','imc_ipd_comorbidity_diag','imc_icd10_code','icd10_ipd_id','icd10_code_id','{code} {name_en}');
-        $crud->setRelationNtoN('ipd_complication_diag','imc_ipd_complication_diag','imc_icd10_code','icd10_ipd_id','icd10_code_id','{code} {name_en}');
+        $crud->setRelationNtoN('ipd_principal_diag', 'imc_ipd_principal_diag', 'imc_icd10_code', 'icd10_ipd_id', 'icd10_code_id', '{code} {name_en}');
+        $crud->setRelationNtoN('ipd_comorbidity_diag', 'imc_ipd_comorbidity_diag', 'imc_icd10_code', 'icd10_ipd_id', 'icd10_code_id', '{code} {name_en}');
+        $crud->setRelationNtoN('ipd_complication_diag', 'imc_ipd_complication_diag', 'imc_icd10_code', 'icd10_ipd_id', 'icd10_code_id', '{code} {name_en}');
         //$crud->setRelationNtoN('ipd_consultation','imc_ipd_consultation','ttr_hims.doctor_name','icd10_ipd_id','doctor_name_id','{fname} {lname} [ {doctor_id} ]');
-        $crud->setRelationNtoN('ipd_other_diag','imc_ipd_other_diag','imc_icd10_code','icd10_ipd_id','icd10_code_id','{code} {name_en}');
+        $crud->setRelationNtoN('ipd_other_diag', 'imc_ipd_other_diag', 'imc_icd10_code', 'icd10_ipd_id', 'icd10_code_id', '{code} {name_en}');
         $output = $crud->render();
         $this->setMyView($output);
     }
-    
+
     public function opd_visit() {
         $crud = $this->acrud;
         $crud->setTable('imc_opd_visit')->setPrimaryKey('hn', 'imc_opd_visit');
@@ -66,6 +72,26 @@ class IMC extends MY_Controller {
         }, true);
         $output = $crud->render();
         $this->setMyView($output);
+    }
+
+    private function _getDoctor() {
+        $this->load->database('theptarin');
+        $query = $this->db->query('SELECT * FROM `doctor`');
+        $rows[] = NULL;
+        foreach ($query->result() as $row) {
+            $rows[$row->doctor_id] = $row->doctor_id . " : " . $row->doctor_name . " : " . $row->category;
+        }
+        return $rows;
+    }
+
+    private function _getPatient() {
+        $this->load->database('theptarin');
+        $query = $this->db->query('SELECT * FROM `patient`');
+        $rows[] = NULL;
+        foreach ($query->result() as $row) {
+            $rows[$row->hn] = $row->hn . " : " . $row->fname . " " . $row->lname;
+        }
+        return $rows;
     }
 
 }
