25560022c433c33f3a530be95c893f1d5a70546f
suchartbu <suchartbu@gmail.com>
2018-08-07 18:44:55 +0700

    update IMC

--- application/controllers/IMC.php
+++ application/controllers/IMC.php
@@ -14,7 +14,7 @@
 class IMC extends MY_Controller {
 
     private $acrud = NULL;
-    public $opd = [];
+    public $opd = NULL;
 
     public function __construct() {
         parent::__construct();
@@ -33,11 +33,13 @@ class IMC extends MY_Controller {
 
     public function icd10_opd($dd = "", $mm = "", $yyyy = "", $vn = "", $hn = "", $doctor_id = "") {
         $this->opd['hn'] = $hn;
-        $this->opd['visit_date'] = $dd . "/" . $mm . "/" . $yyyy;
+        $this->opd['visit_date'] = $yyyy . "-" . $mm . "-" . $dd;
         $this->opd['vn'] = $vn;
         $this->opd['doctor_id'] = $doctor_id;
+        $this->opd['description'] = "รายละเอียดข้อมูล";
         $crud = $this->acrud;
-        $crud->setTable('imc_icd10_opd')->where(['hn' => $hn])->columns(['visit_date', 'vn', 'hn', 'signature_opd']);
+        $crud->setTable('imc_icd10_opd')->where(['hn' => $hn])->columns(['visit_date', 'vn', 'hn', 'signature_opd'])
+                ->addFields(['description', 'opd_principal_diag', 'external_cause']);
         $crud->setRelationNtoN('opd_principal_diag', 'imc_opd_principal_diag', 'imc_icd10_code', 'icd10_opd_id', 'icd10_code_id', '{code} {name_en}');
 
         if ($crud->getState() === 'Initial') {
@@ -47,10 +49,29 @@ class IMC extends MY_Controller {
          * Default value add form
          */
         $crud->callbackAddForm(function ($data) {
-            $data['visit_date'] = $this->opd['visit_date'];
-            $data['hn'] = $this->opd['hn'];
-            $data['vn'] = $this->opd['vn'];
-            $data['signature_opd'] = $this->opd['doctor_id'];
+            $data['description'] = $this->opd['description'];
+            return $data;
+        });
+        $output = $crud->render();
+        $this->setMyView($output);
+    }
+
+    public function icd10_opd_add($dd = "", $mm = "", $yyyy = "", $vn = "", $hn = "", $doctor_id = "") {
+        $this->opd['hn'] = $hn;
+        $this->opd['visit_date'] = $yyyy . "-" . $mm . "-" . $dd;
+        $this->opd['vn'] = $vn;
+        $this->opd['doctor_id'] = $doctor_id;
+        $th_yyyy = $yyyy + 543;
+        $this->opd['description'] = "$dd/$mm/$th_yyyy  VN. $vn HN. " . $this->_getPatient();
+        $crud = $this->acrud;
+        $crud->setTable('imc_icd10_opd')->where(['hn' => $hn])->columns(['hn', 'description'])->unsetEdit()
+                ->setRead()->addFields(['description', 'opd_principal_diag', 'external_cause']);
+        $crud->setRelationNtoN('opd_principal_diag', 'imc_opd_principal_diag', 'imc_icd10_code', 'icd10_opd_id', 'icd10_code_id', '{code} {name_en}');
+        /**
+         * Default value add form
+         */
+        $crud->callbackAddForm(function ($data) {
+            $data['description'] = $this->opd['description'];
             return $data;
         });
         $output = $crud->render();
@@ -78,7 +99,7 @@ class IMC extends MY_Controller {
         //$crud->unsetAdd()->unsetEdit()->unsetDelete();
         $crud->unsetOperations();
         $crud->setActionButton('ICD10', 'fa fa-user', function ($row) {
-            return 'icd10_opd/' . $row->visit_date . '/' . $row->vn . '/' . $row->hn . '/' . $row->doctor_id . '#/add';
+            return 'icd10_opd_add/' . $row->visit_date . '/' . $row->vn . '/' . $row->hn . '/' . $row->doctor_id . '#/add';
         }, true);
         $output = $crud->render();
         $this->setMyView($output);
@@ -96,12 +117,35 @@ class IMC extends MY_Controller {
 
     private function _getPatient() {
         $this->load->database('theptarin');
-        $query = $this->db->query('SELECT * FROM `patient`');
-        $rows[] = NULL;
-        foreach ($query->result() as $row) {
-            $rows[$row->hn] = $row->hn . " : " . $row->fname . " " . $row->lname;
+        $sql = "SELECT * FROM `patient`WHERE `hn` = ?";
+        $query = $this->db->query($sql, [$this->opd['hn']]);
+        $row = $query->row();
+        if (isset($row)) {
+            $sex = ($row->sex == 'M' ) ? 'ชาย' : 'หญิง';
+            $patient = $row->hn . " " . $row->prefix . $row->fname . " " . $row->lname . " เพศ " . $sex;
+            $birthday_date = date_create_from_format('Y-m-d', $row->birthday_date);
+            $diff = date_diff($birthday_date, date_create_from_format('Y-m-d', $this->opd['visit_date']));
+            $th_yyyy = date_format($birthday_date, 'Y') + 543;
+            $patient .= " วันเกิด " . date_format($birthday_date, 'd/m/') . $th_yyyy . " ณ วันที่มาอายุ  " . $diff->format('%y ปี %m เดือน %d วัน');
+        } else {
+            $patient = "ไม่พบ HN. " . $this->opd['hn'];
         }
-        return $rows;
+        return $patient;
+    }
+
+    public function eventBeforeInsert($val_) {
+        switch ($this->OrrACRUD->getTable()) {
+            case 'imc_icd10_opd':
+                $val_->data['visit_date'] = $this->opd['visit_date'];
+                $val_->data['vn'] = $this->opd['vn'];
+                $val_->data['hn'] = $this->opd['hn'];
+                $val_->data['signature_opd'] = $this->opd['doctor_id'];
+                break;
+
+            default:
+                break;
+        }
+        return parent::eventBeforeInsert($val_);
     }
 
 }
